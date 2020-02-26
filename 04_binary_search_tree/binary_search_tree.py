6553863b08b6fca1a16d7df19db2b1c6bc53c3c0
richanynguon <alina.nguon@gmail.com>
2020-02-26 12:42:26 -0500

    complete day one of binary tree

--- 04_binary_search_tree/binary_search_tree.py
+++ 04_binary_search_tree/binary_search_tree.py
@@ -1,7 +1,8 @@
 import sys
-sys.path.append('../queue_and_stack')
+sys.path.append('../02_queue_and_stack')
 from dll_queue import Queue
 from dll_stack import Stack
+
 
 
 class BinarySearchTree:
@@ -12,21 +13,54 @@ class BinarySearchTree:
 
     # Insert the given value into the tree
     def insert(self, value):
-        pass
+        if value < self.value:
+            if not self.left:
+                self.left = BinarySearchTree(value)
+            else:
+                self.left.insert(value)
+        else:
+            if not self.right:
+                self.right = BinarySearchTree(value)
+            else:
+                self.right.insert(value)
 
     # Return True if the tree contains the value
     # False if it does not
     def contains(self, target):
-        pass
+        if self.value == target:
+            return True
+        if self.value < target:
+            if self.right:
+                return self.right.contains(target)
+            else:
+                return False
+        else:
+            if self.left:
+                return self.left.contains(target)
+            else:
+                return False
+       
+
 
     # Return the maximum value found in the tree
+
     def get_max(self):
-        pass
+        max_node = self.value
+        current = self
+        while current:
+            if current.value > max_node:
+                max_node = current.value
+            current = current.right
+        return max_node
 
     # Call the function `cb` on the value of each node
     # You may use a recursive or iterative approach
     def for_each(self, cb):
-        pass
+        cb(self.value)
+        if self.right:
+            self.right.for_each(cb)
+        if self.left:
+            self.left.for_each(cb)
 
     # DAY 2 Project -----------------------
 
