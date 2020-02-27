cfadc990738e58697472008c2ba33140323335a2
richanynguon <alina.nguon@gmail.com>
2020-02-26 20:05:42 -0500

    attempt bubble up in heap sort

--- heap/generic_heap.py
+++ heap/generic_heap.py
@@ -2,9 +2,14 @@ class Heap:
     def __init__(self, comparator):
         self.storage = []
         self.comparator = comparator
+        self.size = 0
 
     def insert(self, value):
-        pass
+        self.storage.append(value)
+        self.size += 1
+        value_idx = (self.size-1)
+        print(self.storage)
+        self._bubble_up(value_idx)
 
     def delete(self):
         pass
@@ -13,10 +18,15 @@ class Heap:
         pass
 
     def get_size(self):
-        pass
+        return self.size
 
     def _bubble_up(self, index):
-        pass
+        parent_idx = index // 2
+        if parent_idx == 0:
+            return parent_idx, index
+        print(parent_idx, index)
+        # while self.storage[parent_idx] < self.storage[index]:
+        #     self.storage[parent_idx], self.storage[index] = self.storage[index], self.storage[parent_idx]
 
     def _sift_down(self, index):
         pass
--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,21 +1,35 @@
 class Heap:
     def __init__(self):
         self.storage = []
+        self.size = 0
 
     def insert(self, value):
-        pass
+        self.storage.append(value)
+        self.size += 1
+        value_idx = self.get_size()
+        self._bubble_up(value_idx-1)
 
     def delete(self):
         pass
 
     def get_max(self):
-        pass
+        return self.storage[0]
 
     def get_size(self):
-        pass
+        return self.size
 
     def _bubble_up(self, index):
-        pass
+        parent_idx = (index // 2)-1
+        print(parent_idx, index, self.storage)
+        if parent_idx <= 0:
+            parent_idx = 0
+
+        while parent_idx >= 0:
+            if self.storage[parent_idx] < self.storage[index]:
+                self.storage[parent_idx], self.storage[index] = self.storage[index], self.storage[parent_idx]
+                self._bubble_up(index)
+            else:
+                break
 
     def _sift_down(self, index):
         pass
